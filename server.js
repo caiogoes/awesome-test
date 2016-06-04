99bc1591071052aede20a5117cdd47828f3e71ab
Caio Navarro <cnavarro@vayon.com.br>
2016-06-04 14:50:15 -0300

    add main page

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express 	= require("express");
 var app 		= express();
 var path 		= require("path");
 var adminRotas 	= express.Router();
+var mainPage 	= express.Router();
 
-adminRotas.get('/', function(req, res){
+mainPage.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/main.html'));
+});
+
+mainPage.get('/fotos', function(req, res){
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -20,6 +25,7 @@ adminRotas.get('/gabriel', function(req, res){
 });
 
 app.use('/fotos', adminRotas);
+app.use('/', mainPage);
 
 app.listen(process.env.PORT || 1337);
 console.log('1337 is the magic port!');
